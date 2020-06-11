1fceef12a9ec4d796dbf6af8bfbb9ea5e6de21a5
Crowdbotics <team@crowdbotics.com>
2020-06-11 20:25:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/singlescave_18029/MainActivity.java
@@ -0,0 +1,15 @@
+package com.singlescave_18029;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "singlescave_18029";
+    }
+}
